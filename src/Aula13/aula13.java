5babc5297012f5dfffa995599bb71363b222f082
perkles <tarcisio.neto@boavistatecnologia.com.br>
2022-08-24 11:17:40 -0300

    Initial commit

---
+++ src/Aula13/aula13.java
@@ -0,0 +1,11 @@
+package Aula13;
+
+public class aula13 {
+    public static void main(String[] args) {
+        //Programa principal
+        Cachorro c = new Cachorro();
+
+
+        c.reagir(false);
+    }
+}
